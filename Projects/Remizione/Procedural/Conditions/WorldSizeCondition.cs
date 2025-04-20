824b318887d073b3b4dbdb123eb330c352cfc703
diegohcanepa <diegohcanepa@gmail.com>
2025-04-20 20:43:34 -0300

    Added WorldSizeCondition

---
+++ Projects/Remizione/Procedural/Conditions/WorldSizeCondition.cs
@@ -0,0 +1,25 @@
+﻿using Engendro;
+
+namespace Remizione
+{
+    /// <summary>
+    /// WorldSizeCondition
+    /// </summary>
+    public sealed class WorldSizeCondition : PlacementCondition
+    {
+        // Constructor
+        public WorldSizeCondition(Int32Range range)
+        {
+            this.Range = range;
+        }
+
+        // IsAvailable
+        public override bool IsAvailable(GameThing thing, WorldBlock block)
+        {
+            return Range.Contains(block.Manager.Blocks.Count);
+        }
+
+        // Range
+        public Int32Range Range { get; }
+    }
+}
